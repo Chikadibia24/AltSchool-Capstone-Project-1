94e2544fd46afdbcabf4a0b90bf8f45094428844
ChikaDibia <ogbuchika71@gmail.com>
2024-05-01 23:29:47 +0100

    Bug Fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,16 +12,33 @@ function closeSideMenu() {
 function openAndCloseMobileMenu() {
   let mobileMenu = document.getElementsByClassName("mobile-nav-container")[0];
   let openCloseIcon = document.getElementsByClassName("open-close-icon")[0];
-  if (mobileMenu.style.height === '0%') {
-    mobileMenu.style.height = '100%';
-    mobileMenu.style.padding = '20px';
+
+  if (mobileMenu.style.height !== '100%') {
+    mobileMenu.style.height = "100%";
     openCloseIcon.innerHTML = "&#x2715;";
+    mobileMenu.style.opacity = "1";
+    mobileMenu.style.padding = "20px";
+    
   } else {
-    mobileMenu.style.height = '0%';
-    mobileMenu.style.padding = "0";
+    mobileMenu.style.height = "0%";
     openCloseIcon.innerHTML = "&#9776;";
+    mobileMenu.style.opacity = "0";
+    mobileMenu.style.padding = "20px";
   }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementsByClassName("fa-bars-container")[0].onclick =
+    openSideMenu;
+
+  document.getElementsByClassName("close-button")[0].onclick = closeSideMenu;
+
+  document.getElementsByClassName("fa-bars-container-mobile")[0].onclick =
+    openAndCloseMobileMenu;
+  
+}
+
+)
 
 
 
