05b9937789481c344fe6bc7ffcb1a8fc429183b8
ChikaDibia <ogbuchika71@gmail.com>
2024-05-01 17:40:49 +0100

    Add Side Menu Bar

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function openSideMenu() {
+  document.getElementsByClassName("side-menu")[0].style.width = `350px`;
+}
+
+function closeSideMenu() {
+  document.getElementsByClassName("side-menu")[0].style.width = `0`;
+}
