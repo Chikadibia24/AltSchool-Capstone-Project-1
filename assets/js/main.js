03a1831fd990b990b0846f00e2ac71ed3df46b7f
ChikaDibia <ogbuchika71@gmail.com>
2024-05-03 19:03:35 +0100

    Add Dynamic Section Update function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
+// Function Change NAV Background On Scroll Starts here
+function changeNavBackground() {
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    document.getElementsByClassName(
+      "nav-bar"
+    )[0].style.backgroundColor = `#000000`;
+    document.getElementsByClassName(
+      "nav-bar"
+    )[0].style.borderBottomColor = `unset`;
+  } else {
+    document.getElementsByClassName(
+      "nav-bar"
+    )[0].style.backgroundColor = `unset`;
+    document.getElementsByClassName(
+      "nav-bar"
+    )[0].style.borderBottomColor = `#1B1B1C`;
+  }
+}
+// Function Change NAV Background On Scroll Starts here
+
+
 // Function Open and Close Side Menu Navigation Starts here
 function openSideMenu() {
   document.getElementsByClassName("side-menu")[0].style.width = `300px`;
@@ -14,12 +35,11 @@ function openAndCloseMobileMenu() {
   let mobileMenu = document.getElementsByClassName("mobile-nav-container")[0];
   let openCloseIcon = document.getElementsByClassName("open-close-icon")[0];
 
-  if (mobileMenu.style.height !== '100vh') {
+  if (mobileMenu.style.height !== "100vh") {
     mobileMenu.style.height = "100vh";
     openCloseIcon.innerHTML = "&#x2715;";
     mobileMenu.style.opacity = "1";
     mobileMenu.style.padding = "20px";
-    
   } else {
     mobileMenu.style.height = "0vh";
     openCloseIcon.innerHTML = "&#9776;";
@@ -30,32 +50,31 @@ function openAndCloseMobileMenu() {
 // Function Open and Close Mobile Menu Navigation Stops here
 
 
+// Function Dynamic Section Update Starts here
+function dynamicPageUpdate(div){
+    document.querySelectorAll(
+      ".about-info-display-section-sub-container-child-2"
+    ).forEach(item => {
+      item.style.display = "none";
+    });
+  
+    document.querySelector(`#${div}`).style.display = "initial";
+  
+  }
+// Function Dynamic Section Update Stops here
+
+
 // Function Scroll To The Top Starts here
-    function scrollToTop() {
-      document.body.scrollTop = `1px`;
-    }
+function scrollToTop() {
+  document.body.scrollTop = `1px`;
+}
 // Function Scroll To The Top ends here
 
 
-// Function Change NAV Background On Scroll Starts here
-    function changeNavBackground() {
-      if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-        document.getElementsByClassName("nav-bar")[0].style.backgroundColor = `#000000`;
-        document.getElementsByClassName(
-          "nav-bar"
-        )[0].style.borderBottomColor = `unset`;
-      } else {
-        document.getElementsByClassName("nav-bar")[0].style.backgroundColor = `unset`;
-        document.getElementsByClassName(
-          "nav-bar"
-        )[0].style.borderBottomColor = `#1B1B1C`;
-      }
-    }
-// Function Change NAV Background On Scroll Starts here
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   document.getElementsByClassName("fa-bars-container")[0].onclick =
     openSideMenu;
 
@@ -63,15 +82,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
   document.getElementsByClassName("fa-bars-container-mobile")[0].onclick =
     openAndCloseMobileMenu;
-  
+
   document.getElementsByClassName("footer-div-yellow-bg")[0].onclick =
     scrollToTop;
+
+  window.onscroll = function () {
+    changeNavBackground();
+  };
+
+  document.querySelectorAll(".bio-data-button").forEach(item => {
+    item.onclick = function () {
+      dynamicPageUpdate(this.dataset.div);
+    }
+  });
   
-  window.onscroll = function () {changeNavBackground()};
-})
-
-
-
-
-
-
+});
